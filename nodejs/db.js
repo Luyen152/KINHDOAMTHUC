c834b6ab1990f6e67d35450bd2c9e13b108a3a1f
Luyen152 <nvl0932498215@gmail.com>
2026-03-14 10:08:58 +0700

    fix database connection

--- nodejs/db.js
+++ nodejs/db.js
@@ -1,12 +1,11 @@
-// backend/db.js
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
 
   waitForConnections: true,
   connectionLimit: 10,
